00feecbf33092a679cd6c5ff0004b54cf627183b
unreisxz <reisxzoliveros@gmail.com>
2026-05-12 00:38:31 +0800

    improve_design_1.0

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-fallback-change-in-
 
 # --- MySQL Database Connection ---
 MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
-MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'rhyan12')  # ← change this
+MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'Andreison#0806')  # ← change this
 MYSQL_HOST = os.environ.get('MYSQL_HOST', '127.0.0.1')
 MYSQL_PORT = os.environ.get('MYSQL_PORT', '3306')
 MYSQL_DB = os.environ.get('MYSQL_DB', 'artisan_db')
